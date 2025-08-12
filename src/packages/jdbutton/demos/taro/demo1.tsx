03f77a91ad606f0369624846053b08f816b32116
zhuqingan.3 <muyouz@qq.com>
2025-08-12 11:28:24 +0800

    feat: 增加最基础的dome

---
+++ src/packages/jdbutton/demos/taro/demo1.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import JDButton from '../../index.taro'
+
+const Demo1 = () => {
+  return (
+    <div className="demo">
+      <div style={{ padding: '16px' }}>
+        <JDButton
+          text="主要按钮"
+          styleCode="btn_primary_fill_normal"
+          onClick={() => console.log('点击了主要按钮')}
+        />
+      </div>
+      <div style={{ padding: '16px' }}>
+        <JDButton
+          text="次要按钮"
+          styleCode="btn_secondary_fill_normal"
+          onClick={() => console.log('点击了次要按钮')}
+        />
+      </div>
+      <div style={{ padding: '16px' }}>
+        <JDButton
+          text="禁用状态"
+          styleCode="btn_primary_fill_normal"
+          buttonEnable={false}
+          onClick={() => console.log('这个按钮被禁用了')}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Demo1
